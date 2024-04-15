94899a3fcf72eab3af4f6009d89152bcd65eb35e
Yan Menephyl <87574460+Menephyl@users.noreply.github.com>
2024-04-15 18:00:12 -0300

    JS COMMENT

---
+++ src/index.js
@@ -0,0 +1,14 @@
+/*
+TESTE DE MESA LÓGICA 
+
+1- Quando o usuário clicar no botão de mostrar mais, deve abrir os projetos que estão escondidos no html
+
+    1.1 - Pegar o botão mostrar mais no js para poder verificar quando o usuário clicar em cima dela 
+    1.2 - Identificar  o clique do usuário
+    1.3 Adicionar a classe "ativo" nos projetos escondidos 
+
+2 - Esconder o botão mostrar mais 
+    1- pegar o botão e esconder ele
+
+
+*/
